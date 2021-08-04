925f1cd1e68b103899d00c61a107e4f1b048259e
winklant000 <winklant000@warren.k12.in.us>
2021-08-04 11:51:48 -0400

    Completed L1 and L2 Notes

--- src/L2.java
+++ src/L2.java
@@ -1,4 +1,17 @@
 public class L2 {
 
     //add the main method header
+    public static void main(String[] args) {
+        String firstName = "George";
+        int age = 16;
+        System.out.println(firstName);
+        System.out.println(age);
+
+        String accountName = "William, Stacy";
+        double accountValue = 200.0;
+        int accountNumber = 1234567;
+        System.out.println(accountName);
+        System.out.println(accountNumber);
+        System.out.print(accountValue);
+    }
 }
