49ae2c58d117f68ec8cd76eea09ba9db85cd59d2
winklant000 <winklant000@warren.k12.in.us>
2021-08-09 12:27:54 -0400

    Completed L1 and L2 Notes

--- src/L4.java
+++ src/L4.java
@@ -1,2 +1,17 @@
 public class L4 {
+
+    public static void main(String[] args){
+
+        int value = 5;
+        int value2 = 10;
+        System.out.println(value + value2);
+
+        value = 6;
+        System.out.println(value + value2);
+
+        //Bad Example - Data Types Don't Match
+        //value = 7.2;
+
+    }
+
 }
